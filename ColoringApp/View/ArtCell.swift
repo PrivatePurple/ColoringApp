06d7121dec99076515a92a757ce2de6255806224
Keenan <Keenanking555@gmail.com>
2018-11-07 13:36:08 -0700

    Linked up IBOutlets

---
+++ ColoringApp/View/ArtCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArtCell.swift
+//  ColoringApp
+//
+//  Created by Davis, Keenan on 11/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    @IBOutlet weak var artImage: UIImageView!
+    @IBOutlet weak var artLabel: UILabel!
+}
